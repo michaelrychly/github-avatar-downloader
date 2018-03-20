b56199442a5221318eb90d70c81937eca70b9ff2
michaelrychly <MichaelRychly@gmail.com>
2018-03-19 20:57:03 -0400

    with comments

--- download_avatars.js
+++ download_avatars.js
@@ -6,12 +6,16 @@ var repo = process.argv[3];
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+//main function retrieve repository contributors
 function getRepoContributors(owner, repo, cb) {
+  //check for mandatory command line arguments owner and repository
+  //if they are provided continue with retrieving the avatar
   if (!owner) {
     console.log("Please provide an owner!");
   } else if (!repo){
     console.log("Please provide a repository!");
   } else{
+    //http header
     var options = {
       url: "https://api.github.com/repos/" + owner + "/" + repo + "/contributors",
       headers: {
@@ -20,6 +24,7 @@ function getRepoContributors(owner, repo, cb) {
       }
     };
 
+    //http request with callback function parsing the result
     request(options, function(err, res, body) {
       var obj = JSON.parse(body);
       cb(err, obj);
@@ -27,24 +32,30 @@ function getRepoContributors(owner, repo, cb) {
   }
 };
 
+//retrieve the contributors
 getRepoContributors(owner, repo, function(err, result) {
   console.log("Errors:", err);
 
+  //check if the folder avatars exists
   if (!fs.exists('avatars')){
+    //if it does not exist create the folder
     fs.mkdir('avatars',function(err){
       console.log("Directory created successfully!");
     });
   };
 
+  //download the image
   downloadImageByURL(repo, owner);
 });
 
+//download image function taking url and path as argument
 function downloadImageByURL(url, filePath) {
 
   request.get(url + filePath)               // Note 1
        .on('error', function (err) {                                   // Note 2
          throw err;
        })
+       //write the image to the folder
        .pipe(fs.createWriteStream('./avatars/' + filePath));
 }
 
